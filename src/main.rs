ce9ee048a22a0001ca442e13601d27f81db71a0c
aza547 <alexander-12345@hotmail.co.uk>
2024-02-03 10:14:28 +0000

    tidy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use clap::{Parser, Subcommand};
-use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 use chrono::prelude::*;
-use std::process;
 use regex::Regex;
 
 /// Automated changelog version bumping tool.
@@ -26,7 +24,7 @@ enum Commands {
 }
 
 fn main() {
-  let cli = Cli::parse();
+  let cli: Cli = Cli::parse();
 
   match cli.command {
     Some(c) => bump(c),
@@ -35,31 +33,14 @@ fn main() {
 }
 
 fn bump(flavor: Commands) {
-  println!("{:?}", flavor);
+  let path = "CHANGELOG.md";
 
-  // TODO search recursively for this
-  let path = "docs/CHANGELOG.md";
-
-  if let Ok(metadata) = fs::metadata(path) {
-    if !metadata.is_file() {
-      println!("Not a file.");
-      return;
-    }
-  } else {
-    println!("No CHANGELOG.md found.");
-    return;
-  }
-
-  let lines: Result<Vec<String>, io::Error> = read_file_lines(path);
-
-  let lines: Vec<String> = lines.unwrap_or_else(|err| {
-    eprintln!("Error reading the file: {}", err);
-    process::exit(1);
-  });
-
-  let mut last_version: Option<String> = None;
+  let lines: Vec<String> = read_file_lines(&path)
+    .expect("Error reading file");
   
-  let mut last: &str = "";
+  let mut last_line: &str = "";
+  let mut last_version: &str = "";
+
   let mut changed_empty = true;
   let mut added_empty = true;
   let mut fixed_empty = true;
@@ -67,30 +48,29 @@ fn bump(flavor: Commands) {
   let regex_pattern = Regex::new(r"## \[(\d+.\d+.\d+)\] -.*").unwrap();
 
   for line in lines.iter() {
-    if last == "### Changed" && line.starts_with("-") {
+    if last_line == "### Changed" && line.starts_with("-") {
       changed_empty = false;
     } 
 
-    if last == "### Added" && line.starts_with("-") {
+    if last_line == "### Added" && line.starts_with("-") {
       added_empty = false;
     } 
 
-    if last == "### Fixed" && line.starts_with("-") {
+    if last_line == "### Fixed" && line.starts_with("-") {
       fixed_empty = false;
     } 
     
     if let Some(captures) = regex_pattern.captures(line) {
-      last_version = Some(captures.get(1).unwrap().as_str().to_owned());
+      last_version = captures.get(1).unwrap().as_str();
     }
-
-    last = line;
 
     if line.starts_with("## [") {
       break;
     }
+
+    last_line = line;
   }
 
-  let last_version: String = last_version.expect("latest_version is None");
   let mut output_lines: Vec<String> = Vec::new();
   let mut passed_last_release: bool = false;
 
@@ -104,7 +84,7 @@ fn bump(flavor: Commands) {
 
       // Update the current unreleased section to reflect the new version and date.
       let date: String = get_formatted_date();
-      let new_version = get_new_version(&flavor, &last_version);
+      let new_version = get_new_version(&flavor, last_version);
       let prefix: String = format!("## [{}] - ", new_version).to_owned();
       output_lines.push(prefix + &date);
     } else if !passed_last_release && line == "### Changed"{
@@ -121,27 +101,21 @@ fn bump(flavor: Commands) {
     }
   }
 
-  for (_, line) in output_lines.iter().enumerate().take(20) {
-    println!("{}", line);
-  }
-
   rewrite_changelog(&path.to_string(), &output_lines);
 }
 
 fn read_file_lines(file_path: &str) -> io::Result<Vec<String>> {
   let file = File::open(file_path)?;
   let reader = io::BufReader::new(file);
-  let lines: io::Result<Vec<String>> = reader.lines().collect();
-  return lines;
+  reader.lines().collect()
 }
 
 fn get_formatted_date() -> String {
   let local: DateTime<Local> = Local::now();
-  let formatted_date: String = local.format("%Y-%m-%d").to_string();
-  return formatted_date;
+  local.format("%Y-%m-%d").to_string()
 }
 
-fn get_new_version(flavor: &Commands, last: &String) -> String {
+fn get_new_version(flavor: &Commands, last: &str) -> String {
   let pattern = Regex::new(r"(\d+)\.(\d+)\.(\d+)").unwrap();
 
   let mut major: i32 = 0;
@@ -160,7 +134,7 @@ fn get_new_version(flavor: &Commands, last: &String) -> String {
     Commands::Patch => patch += 1,
   }
 
-  return format!("{}.{}.{}", major, minor, patch);
+  format!("{}.{}.{}", major, minor, patch)
 }
 
 fn rewrite_changelog(path: &String, lines: &Vec<String>) {
